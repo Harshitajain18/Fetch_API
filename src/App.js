a532fd7d1c24d3f52f3c88019ffa9a205a984200
Harshita Jain <hjain0457@gmail.com>
2024-09-06 16:45:20 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './style.scss';
+import {useState } from 'react';
+
 
 function App() {
+  const [userName, setUserName] = useState(null);
+  const [data,setUserData] = useState([]);
+
+  const onSubmit = () => {
+    const fetching = fetch(`https://api.github.com/users/${userName}`).then((response)=>{
+      response.json().then((result)=>{
+        setUserName(result);
+      })
+    }) 
+  }
+
+  console.log("userName: ", userName);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='github-project'>
+        <button className='github-project__homebtn'>Home</button>
+        <h1 className='github-project__heading'>
+          GitHub followers project
+        </h1>
+        
+        <div className='github-project__users'>
+          <h1 className='github-project__users__title'>GitHub Details</h1>
+           <label>Enter GitHub Username : </label>
+           <input type='text' onChange={(e)=>setUserName(e.target.value)}></input> 
+           <button className='github-project__users__submitbtn' onClick={onSubmit}>Submit</button>
+           <p className='github-project__users__loc'>Location :</p>
+
+          <div className='github-project__users__box'>
+            {data.map((item)=>{
+             <> 
+               <p>{item.name}</p>
+               <p>{item.followers}</p>
+             </>
+            })}
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
